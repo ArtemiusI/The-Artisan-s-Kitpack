241a00110c2d207852a1f253181eae263686b34f
ArtemiusI <whuangx10@gmail.com>
2022-05-09 20:34:10 -0400

    v2.2

---
+++ ArtisansKitpack/no_overwrite/m_meexsp.lua
@@ -0,0 +1,11 @@
+me_numKitsChecked = 0
+
+-- A kit in this list can only learn priest spells that have the kit under kit_include in the me_addSpellChoice list, regardless of what class_include says.
+me_excludeAllNormalPriestSpellsFromTheseKits = {}
+
+-- A kit in this list can only learn wizard spells that have the kit under kit_include in the me_addSpellChoice list, regardless of what class_include says.
+me_excludeAllNormalWizardSpellsFromTheseKits = {}
+
+-- New spell choices should be put in this list.
+me_addSpellChoice = {
+}
