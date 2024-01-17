fe01a62b59386f4f370aefe1c401e8480c6048ee
ArtemiusI <whuangx10@gmail.com>
2024-01-17 09:25:33 -0500

    v4.5a
    Made quality of life changes to Sacred Fist and Enlightened Fist
    Fixed thief action bar issues
    Moved Sorcerer kits to top of order to try and mitigate issues with spell selection

--- ArtisansKitpack/lua/m_c0thf2.lua
+++ ArtisansKitpack/lua/m_c0thf2.lua
@@ -1,15 +1,42 @@
 function ArtisanSwashbucklerActionbarListener(config, state)
    if 
       state == 4
-	  or state == 9
-	  or state == 10
-	  or state == 13
       and EEex_GameObject_GetSelected():getActiveStats().m_nKit == 0x400C
       then
       EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.GUARD)
    end
 end
 EEex_Actionbar_AddListener(ArtisanSwashbucklerActionbarListener)
+
+function ArtisanSwashbucklerFighterActionbarListener(config, state)
+   if 
+      state == 9
+      and EEex_GameObject_GetSelected():getActiveStats().m_nKit == 0x400C
+      then
+      EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.GUARD)
+   end
+end
+EEex_Actionbar_AddListener(ArtisanSwashbucklerFighterActionbarListener)
+
+function ArtisanSwashbucklerFighterMageActionbarListener(config, state)
+   if 
+      state == 10
+      and EEex_GameObject_GetSelected():getActiveStats().m_nKit == 0x400C
+      then
+      EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.GUARD)
+   end
+end
+EEex_Actionbar_AddListener(ArtisanSwashbucklerFighterMageActionbarListener)
+
+function ArtisanSwashbucklerMageActionbarListener(config, state)
+   if 
+      state == 13
+      and EEex_GameObject_GetSelected():getActiveStats().m_nKit == 0x400C
+      then
+      EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.GUARD)
+   end
+end
+EEex_Actionbar_AddListener(ArtisanSwashbucklerMageActionbarListener)
 
 function ArtisanSwashbucklerClericActionbarListener(config, state)
    if 
