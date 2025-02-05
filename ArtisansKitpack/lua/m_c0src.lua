62c3bc733313fe6e8e3ebed634805c12ba8212be
ArtemiusI <whuangx10@gmail.com>
2025-02-06 02:59:57 +0800

    v5.4
    Arcane Trickster changes - backstab penalty replaced with THAC0 progression penalty (advances as a mage), sorcerer variant (EEex only) added

---
+++ ArtisansKitpack/lua/m_c0src.lua
@@ -0,0 +1,11 @@
+function ArtisanSorcererTricksterActionbarListener(config, state)
+   if 
+      state == 1
+      and EEex_GameObject_GetSelected():getActiveStats().m_nKit == 0x4000
+      then
+      EEex_Actionbar_SetButton(3, EEex_Actionbar_ButtonType.FIND_TRAPS)
+      EEex_Actionbar_SetButton(4, EEex_Actionbar_ButtonType.THIEVING)
+      EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.STEALTH)
+   end
+end
+EEex_Actionbar_AddListener(ArtisanSorcererTricksterActionbarListener)
