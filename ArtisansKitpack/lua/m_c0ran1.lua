3c0367b568eb8d8ec1d6a51bdea45c1a904b99d5
ArtemiusI <whuangx10@gmail.com>
2025-03-10 05:38:32 +0800

    improved code on rangers

--- ArtisansKitpack/lua/m_c0ran1.lua
+++ ArtisansKitpack/lua/m_c0ran1.lua
@@ -6,3 +6,13 @@ function C0RangerActionbarListener(config, state)
    end
 end
 EEex_Actionbar_AddListener(C0RangerActionbarListener)
+
+function C0RangerActionbarListener(config, state)
+   if 
+      state == 18
+      then
+      EEex_Actionbar_SetButton(4, EEex_Actionbar_ButtonType.FIND_TRAPS)
+      EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.STEALTH)
+   end
+end
+EEex_Actionbar_AddListener(C0RangerActionbarListener)
