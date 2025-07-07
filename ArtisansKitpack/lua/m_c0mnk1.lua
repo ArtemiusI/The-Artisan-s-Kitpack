5fa795c9001369855c30ab1148789990e9ef440e
ArtemiusI <whuangx10@gmail.com>
2025-07-08 03:06:14 +0800

    Mystic Fist changes
    Tentative changes to Sacred Fist:
    - Sacred Fist may now dual-class to mage or thief
    - Sacred and Enlightened Fists may now use staves
    - Behind the scenes changes to fist mechanics - enchanted fists disappear upon dual-classing until inactive class is restored. Equipping 'normal' fists automatically switches to enchanted fists
    Monks wielding staves no longer have visible helmets

--- ArtisansKitpack/lua/m_c0mnk1.lua
+++ ArtisansKitpack/lua/m_c0mnk1.lua
@@ -3,11 +3,20 @@ function MoClericMonkActionbarListener(config, state)
       state == 3
       and EEex_GameObject_GetSelected():getActiveStats().m_nKit == 0x4000
       then
-      EEex_Actionbar_SetButton(1, EEex_Actionbar_ButtonType.QUICK_WEAPON_3)
-      EEex_Actionbar_SetButton(2, EEex_Actionbar_ButtonType.QUICK_WEAPON_1)
-      EEex_Actionbar_SetButton(3, EEex_Actionbar_ButtonType.QUICK_WEAPON_2)
+      EEex_Actionbar_SetButton(3, EEex_Actionbar_ButtonType.QUICK_WEAPON_3)
       EEex_Actionbar_SetButton(4, EEex_Actionbar_ButtonType.FIND_TRAPS)
       EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.TURN_UNDEAD)
    end
 end
 EEex_Actionbar_AddListener(MoClericMonkActionbarListener)
+
+function MoClericMageMonkActionbarListener(config, state)
+   if 
+      state == 14
+      and EEex_GameObject_GetSelected():getActiveStats().m_nKit == 0x4000
+      then
+      EEex_Actionbar_SetButton(3, EEex_Actionbar_ButtonType.FIND_TRAPS)
+      EEex_Actionbar_SetButton(4, EEex_Actionbar_ButtonType.TURN_UNDEAD)
+   end
+end
+EEex_Actionbar_AddListener(MoClericMageMonkActionbarListener)
